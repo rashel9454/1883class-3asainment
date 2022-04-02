09664e7dcdb841a9446d1c2609dbe6610f089213
rashel9454 <rashelrana931@gmail.com>
2022-04-03 00:03:34 +0600

    02/04/2022

--- indext.php
+++ indext.php
@@ -9,28 +9,27 @@ $SubjectD = 65;
 $total_marks = $SubjectA = 72+ $SubjectB+$SubjectC+$SubjectD;
 $average_mark = $total_marks /4; 
 
-echo "average Mark: $average_mark";
+echo "average mark: $average_mark";
 
 echo "<br>";
 $marks =  $average_mark;
 
-if( $marks <= 32 ){
-    echo "result :F grade";
-}elseif( ($marks >= 33) && ($marks <= 39) ){
-    echo "result  grade";
-}elseif( ($marks >= 40) && ($marks <= 49) ){
-    echo "result :C grade";
-}elseif( ($marks >= 50) && ($marks <= 59) ){
-    echo "result :B grade";
-}elseif( ($marks >= 60) && ($marks <= 69) ){
-    echo "result :A- grade";
-}elseif( ($marks >= 70) && ($marks <= 79) ){
-    echo "result :A grade";
-}elseif( ($marks >= 80) && ($marks <= 100) ){
-    echo "result :C grade";
-}else{
-   echo "invalid Number";
-}
+if( $marks <= 32 )
+{ echo "result :F grade";}
+elseif( ($marks >= 33) && ($marks <= 39) )
+{echo "result  grade";}
+elseif( ($marks >= 40) && ($marks <= 49) )
+{echo "result :C grade";}
+elseif( ($marks >= 50) && ($marks <= 59) )
+{echo "result :B grade";}
+elseif( ($marks >= 60) && ($marks <= 69) )
+{echo "result :A- grade";}
+elseif( ($marks >= 70) && ($marks <= 79) )
+{echo "result :A grade";}
+elseif( ($marks >= 80) && ($marks <= 100) )
+{echo "result :C grade";}
+else{echo "invalid Number";}
+
 echo "<br>" ;
 //Question 02//
 //Let’s make a game with your bike… You are riding a bike, your bike has some functionalities, these are… start, stop, break, gear, signal, headlight.//
